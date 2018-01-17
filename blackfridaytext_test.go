a7aa8f1703e1a270e341ac0679b36d6c6dc9a0f9
Greg Holt <gregory.holt@gmail.com>
2018-01-17 13:58:14 -0800

    wip

--- blackfridaytext_test.go
+++ blackfridaytext_test.go
@@ -8,7 +8,7 @@ import "testing"
 
 func TestBasic(t *testing.T) {
 	in := "Basic Test"
-	out := string(MarkdownToTextNoMetadata([]byte(in), nil))
+	out := string(MarkdownToTextNoMetadata([]byte(in), &Options{Width: 80}))
 	exp := "Basic Test\n"
 	if out != exp {
 		t.Errorf("%#v != %#v", out, exp)
